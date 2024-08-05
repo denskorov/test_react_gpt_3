9145bf6a6e691845506ff9bfd2c1d38237a47116
Denys Skorov <denys.s@sterl.in>
2024-08-05 13:39:18 +0300

    added files

---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {atlassian, dropbox, google, shopify, slack} from './imports';
+import './brand.css';
+
+const Brand = () => (
+    <div className="gpt3__brand section__padding">
+        <div>
+            <img src={google} alt="Google Brand" />
+        </div>
+        <div>
+            <img src={slack} alt="slack"/>
+        </div>
+        <div>
+            <img src={atlassian} alt="atlassian"/>
+        </div>
+        <div>
+            <img src={dropbox} alt="dropbox"/>
+        </div>
+        <div>
+            <img src={shopify} alt="shopify"/>
+        </div>
+    </div>
+);
+
+export default Brand;
